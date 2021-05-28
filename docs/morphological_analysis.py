e4b6030c2dc3468f6485f7eb3556785156b0a6a8
yamadori0617 <bjtk1290202@gn.iwasaki.ac.jp>
2021-05-29 05:49:46 +0900

    fix morphological_analysis.py

--- docs/morphological_analysis.py
+++ docs/morphological_analysis.py
@@ -14,7 +14,6 @@ def parse_tweets(tweets: list) -> list:
         tweet_text = re.sub(r"http\S+", "", tweet_text)
         tweet_text = re.sub(r'(https?://[\w/:%#\$&\?\(\)~\.=\+\-]+)', '', tweet_text)
         tweet_text = re.sub(r'[!-/:-@＠️️★☆*＊#＃️[-`{-~¢-¿“-⟿⬀-⯐、、。　-〶\u3099-ゞ・-ヾ㈠-㏿︐-﹪！-／：-［-｀｛-･ﾞﾟ￥|｜「」《》≪≫【】《》]', '', tweet_text)
-        # parse_texts.append(tagger.parse(tweet_text).strip().split())
         parse_texts.append(tagger.parse(tweet_text).strip())
     return parse_texts
 
@@ -26,7 +25,6 @@ def parse_text(text: str) -> list:
 def main() -> None:
     text = input("text: ")
     print("\n{}".format(parse_text(text)))
-
 
 if __name__ == "__main__":
     main()
