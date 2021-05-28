95e105d301fc92a92263d503a7b1938a0b459291
yamadori0617 <bjtk1290202@gn.iwasaki.ac.jp>
2021-05-29 05:46:46 +0900

    add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+setup(
+    name='tweeting_emotions',
+    version='0.0.1',
+    description='Analyze the sentiment towards keywords from twitter.',
+    long_description=readme,
+    author='Yamadori0617',
+    url='https://github.com/yamadori0617/tweeting_emotions',
+    license=license,
+    packages=find_packages(exclude=('tests', 'docs'))
+)
