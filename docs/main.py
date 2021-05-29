bbaae7cf00bdf67039b9c46d41c47bf98584decc
yamadori0617 <bjtk1290202@gn.iwasaki.ac.jp>
2021-05-29 23:10:24 +0900

    update main.py

--- docs/main.py
+++ docs/main.py
@@ -7,7 +7,7 @@ from emotion_scores import tweets_scoring
 def main() -> None:
     try:
         keyword = input("keyword: ")
-        count = int(input("count: "))
+        count = int(input("searches count: "))
 
         tweets = get_tweets(keyword, count)
         emotion_sum_scores, positive_words, negative_words = tweets_scoring(tweets)
@@ -25,8 +25,8 @@ def main() -> None:
             else:
                 normal_ratio += 1
 
-        print(positive_words_counter)
-        print(negative_words_counter)
+        print("positive words: {}".format(positive_words_counter))
+        print("negative_words: {}".format(negative_words_counter))
         print("-------------------------------")
         print("positive: {}/{}".format(positive_ratio, count))
         print("negative: {}/{}".format(negative_ratio, count))
